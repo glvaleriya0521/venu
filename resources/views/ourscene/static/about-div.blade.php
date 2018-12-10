ab7827f8dafab44f69bc2a171750b093a1d287e5
GLB Design & Development <45498842+GLBDD@users.noreply.github.com>
2018-12-09 16:48:41 -0800

    Update to About Us to Remove OurScene References
    Update to About Us to Remove OurScene References

--- resources/views/ourscene/static/about-div.blade.php
+++ resources/views/ourscene/static/about-div.blade.php
@@ -8,22 +8,22 @@
 				What is VenU?
 			</span>
 			<p class="col s12 m12 l12">
-				VenU is for artists and venues. 
-				It is your life as an artist or venue online! 
-				Use it to connect your network and to discover new spaces and collaborators. 
-				Plan and book shows or look for open slots, 
-				with VenU you can manage your entire performance calendar all in one place. 
-				Use messaging to discuss upcoming shows or take it further and use it to find inspiration and collaboration. 
-				Most of all: make OurScene <i>yours</i>.
+				VenU is for artists and venues.
+				It is your life as an artist or venue online!
+				Use it to connect your network and to discover new spaces and collaborators.
+				Plan and book shows or look for open slots,
+				with VenU you can manage your entire performance calendar all in one place.
+				Use messaging to discuss upcoming shows or take it further and use it to find inspiration and collaboration.
+				Most of all: make VenU <i>yours</i>.
 			</p>
-		</div>		
+		</div>
 		<div class="row">
 			<span class="col s12 m12 l12 section-title">
 				Mission
 			</span>
 			<p class="col s12 m12 l12">
-				VenU's mission is to make booking as simple as the push of a button. 
-				Being artists and small business owners ourselves, we have one goal: to create 
+				VenU's mission is to make booking as simple as the push of a button.
+				Being artists and small business owners ourselves, we have one goal: to create
 				a simple platform for artists and venues to discover one another and book shows.
 			</p>
 		</div>
@@ -32,11 +32,11 @@
 				History
 			</span>
 			<p class="col s12 m12 l12">
-				VenU’s founding team is comprised of five entrepreneurial, 
-				artistic spirits who have been working dutifully to achieve 
-				our mission since 2014. We are proud to represent you --  
-				the artist and the small business owner -- 
-				and provide a worthy service that benefits you, 
+				VenU’s founding team is comprised of five entrepreneurial,
+				artistic spirits who have been working dutifully to achieve
+				our mission since 2014. We are proud to represent you --
+				the artist and the small business owner --
+				and provide a worthy service that benefits you,
 				your business, your art and your community. Make it yours!
 			</p>
 		</div>
@@ -48,32 +48,32 @@
 				Search:
 			</span>
 			<p class="col s12 m12 l12">
-				OurScene is an open platform and you can always use search to find artists or venues. 
-				Start typing and we’ll do the rest. If you don’t know what you’re looking 
-				for you can browse artists and venues on OurScene right from the homepage.
+				VenU is an open platform and you can always use search to find artists or venues.
+				Start typing and we’ll do the rest. If you don’t know what you’re looking
+				for you can browse artists and venues on VenU right from the homepage.
 			</p>
 			<span class="col s12 m12 l12 sub-section-title">
 				Messaging:
 			</span>
 			<p class="col s12 m12 l12">
-				Use OurScene to directly message any artist or venue. 
-				Manage your messages or start new conversations or group chats by 
+				Use VenU to directly message any artist or venue.
+				Manage your messages or start new conversations or group chats by
 				simply clicking the message bubble in the top right of any screen.
 			</p>
 			<span class="col s12 m12 l12 sub-section-title">
 				Booking:
 			</span>
 			<p class="col s12 m12 l12">
-				Artists may directly suggest a show to a venue by clicking the “Suggest a show” button 
-				and entering details about their desired performance including 
+				Artists may directly suggest a show to a venue by clicking the “Suggest a show” button
+				and entering details about their desired performance including
 				the name of the venue, date and showtime.
 			</p>
 			<p class="col s12 m12 l12">
-				Venues have two options: Start by clicking the “Suggest a show” button. 
-				You will be prompted to enter the details of the show including name, 
-				date and showtimes. Once complete you may invite specific artists to the event. 
-				If you skip that step the show is considered open and artists from the entire 
-				OurScene community can apply to fill your slate. It’s your choice!
+				Venues have two options: Start by clicking the “Suggest a show” button.
+				You will be prompted to enter the details of the show including name,
+				date and showtimes. Once complete you may invite specific artists to the event.
+				If you skip that step the show is considered open and artists from the entire
+				VenU community can apply to fill your slate. It’s your choice!
 			</p>
 		</div>
 		<div class="row">
@@ -81,8 +81,8 @@
 				Our Partners
 			</span>
 			<p class="col s12 m12 l12">
-				Coming soon!  
-				If you are interested in partnering with us, please email <a href="mailto:george@ourscene.network">george@ourscene.network</a> directly.
+				Coming soon!
+				If you are interested in partnering with us, please email <a href="mailto:ourscene.hosting@gmail.com">VenU</a> directly.
 			</p>
 		</div>
 	</div>
