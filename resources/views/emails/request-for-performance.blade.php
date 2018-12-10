daf58e771fd7dced3be9229ee0f3da53a3db6fd8
GLB Design & Development <45498842+GLBDD@users.noreply.github.com>
2018-12-10 10:24:55 -0800

    Update to colors in email for new UI
    Update to colors in email for new UI

--- resources/views/emails/request-for-performance.blade.php
+++ resources/views/emails/request-for-performance.blade.php
@@ -8,7 +8,7 @@ Hi, {{ $venue->name }}!
 
 <br/><br/><br/>
 
-<span style="color: #534d93; font-weight: bold;">Event details</span>
+<span style="color: rgba(26, 120, 246, 1.0); font-weight: bold;">Event details</span>
 
 <br/><br/>
 
@@ -27,7 +27,7 @@ Hi, {{ $venue->name }}!
 
 <br/><br/>
 
-<span style="color: #534d93; font-weight: bold;">Performance time</span>
+<span style="color: rgba(26, 120, 246, 1.0); font-weight: bold;">Performance time</span>
 
 <br/><br/>
 
@@ -41,7 +41,7 @@ Hi, {{ $venue->name }}!
 
 <br/><br/><br/>
 
-You can <a href="{{ action('UserController@getLogin') }}" style="color: #534d93; text-decoration: none;">log in</a> to check this request.
+You can <a href="{{ action('UserController@getLogin') }}" style="color: rgba(26, 120, 246, 1.0); text-decoration: none;">log in</a> to check this request.
 
 <br/><br/><br/><br/>
 
