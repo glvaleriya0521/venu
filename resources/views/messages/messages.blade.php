ff1bc6ddd17eda1755aa298b88503e07c6143827
GLB Design & Development <45498842+GLBDD@users.noreply.github.com>
2018-12-13 19:35:32 -0800

    Update Messages UI
    Update Messages UI

--- resources/views/messages/messages.blade.php
+++ resources/views/messages/messages.blade.php
@@ -13,7 +13,7 @@
 
 <div class="container-fluid">
 	<div class="row">
-		<div id="message-form" class="card col s12 m12 l8 offset-l2 ">
+		<div id="message-form" class="card col s12 m12 l8 offset-l2 " style="border: 1px solid rgba(26, 120, 246, 1.0)">
 	    <div class="card-action title">
 	      <div class="row" style="margin-bottom:0;">
 	      	<div class="col s9">
@@ -32,7 +32,7 @@
 	      <div class="row">
 	        <div id="messages-conversation" class="col s12">
 	          	<ul class="collection" id="messages_container_ul">
-				
+
 				</ul>
 	        </div>
 	      </div>
@@ -43,16 +43,16 @@
 
 <script type="text/javascript">
 	 var user_id = "{{Session::get('id')}}";
-	function getMessageList(){	
+	function getMessageList(){
 		console.log("get message");
 		$.ajax({
 				 url: "{{ action('MessageController@getAjaxHeaderMessages')}}/"+user_id,
-				 type: "GET", 
+				 type: "GET",
 				 dataType: "json",
 				 success: function(data){
-					
+
 					if (data == "false") {
-						
+
 					}else {
 						var messages = data;
 						var $messages_container = $("#messages_container_ul");
@@ -63,11 +63,11 @@
 							var conversation_str = '<li class="collection-item avatar row" style="padding:0 !important">'
 							conversation_str += '<a href='
 							conversation_str += "{{action('MessageController@getMessage','')}}" + "/" + conversation['_id'] + " >";
-							
+
 							conversation_str += '<div class="col s2 m1 l1">'
-							conversation_str += '<img src=' 
+							conversation_str += '<img src='
 							if(conversation['image'] !== null && conversation['image'] != "")
-								conversation_str += conversation['image'] 
+								conversation_str += conversation['image']
 							else{
 								conversation_str += "{{asset('images/icons/profile-pic.png')}}"
 							}
@@ -83,7 +83,7 @@
 							}
 							conversation_str += '>' + conversation['title'] + '</span>'
 							conversation_str += '</div>'
-							
+
 							conversation_str += '<div class="row">'
 							conversation_str += '<p class="message col s12 m12 l12">'
 							conversation_str += '<span '
