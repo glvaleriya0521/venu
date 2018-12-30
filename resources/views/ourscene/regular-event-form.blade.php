c75d9ae23ef9a1c257af8c1839ee1c52b240c985
GLBDD <Hello@glbdd.com>
2018-12-30 12:33:58 +0800

    promotion ui fixed

--- resources/views/ourscene/regular-event-form.blade.php
+++ resources/views/ourscene/regular-event-form.blade.php
@@ -367,6 +367,7 @@ Required items are indicated with <span class="required-color">*</span>.
 <div style="display: inline; margin-left: 35%;">
 <div class="fb-share-button" data-href="https://testvenu.glbdd.com" data-layout="button_count" data-size="large" data-mobile-iframe="true"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse?text=aaa" class="fb-xfbml-parse-ignore" style="margin-left: 50%;"><i class="fa fa-facebook" style="font-size: 20px;"></i>Share</a></div>
 </div>
+
 <a class="twitter-share-button" target="_blank" 
   href="https://twitter.com/intent/tweet"
   data-size="large"><i class="fa fa-twitter" style="font-size: 20px;"></i>
