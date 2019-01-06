d794871f0a558f2738445bdeaede62e324315080
GLB Design & Development <45498842+GLBDD@users.noreply.github.com>
2019-01-06 02:50:19 -0800

    Update Spacing for Mobile UI on Nav Buttons
    Update Spacing for Mobile UI on Nav Buttons

--- resources/views/navs/main-navigation-3.blade.php
+++ resources/views/navs/main-navigation-3.blade.php
@@ -3,9 +3,9 @@
 	@foreach($items as $item)
 		<a href="{{ $item['url'] }}" class="btn btn-default @if(Request::url() == $item['url']) active @endif" style="text-transform: none; font-size: 12px;">
 		@if(Request::url() == $item['url'])
-			<img src="{{ $item['image-active'] }}"/>
+			<img src="{{ $item['image-active'] }}" style="margin-right: 1px;"/>
 		@else
-			<img src="{{ $item['image'] }}"/>
+			<img src="{{ $item['image'] }}" style="margin-right: 1px;"/>
 		@endif
 			{{ $item['text'] }}
 			<?php
@@ -13,9 +13,9 @@
             	$pending_requests_counter = count($pending_requests);
 		 	?>
 		 	@if($pending_requests_counter > 0 && $item['text'] == "Requests")
-		 		<span class="badge red"> {{$pending_requests_counter}}</span>
+		 		<span class="badge red" style="margin-left: 2px;"> {{$pending_requests_counter}}</span>
 		 	@endif
 		</a>
 	@endforeach
 	</div>
-</div>	
+</div>
